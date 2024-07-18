eade6246102cab317be3e2f4d1b022ecc4b960ff
SchadenKai <kairustecsonpersonal@gmail.com>
2024-07-18 22:16:02 +0800

    transformed user, pets, and roles schema into SQLModels

--- src/users/models.py
+++ src/users/models.py
@@ -1,39 +1,11 @@
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, ForeignKey, ARRAY, UUID
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
+from .schemas import UsersBase, PetsBase
+from sqlmodel import Field, Relationship
 
-Base = declarative_base()
+class Users(UsersBase, table=True):
+    id : int | None = Field(default=None, primary_key=True)
+    pets : list[PetsBase] = Relationship(back_populates="owner")
 
-class Pet(Base):
-    __tablename__ = 'pet'
-    
-    pet_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    pet_name = Column(String(100), nullable=False)
-    age = Column(Integer)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.user_id'))  # To be added later
-    type = Column(String(50))
-    breed = Column(String(100))
-    
-    bookings = relationship("Booking", back_populates="pet")
-
-class Users(Base):
-    __tablename__ = 'users'
-
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    first_name = Column(String(106))
-    last_name = Column(String(108))
-    email = Column(String(100))
-    password = Column(String(168))
-    address = Column(String(108))
-    phone_number = Column(String(100))
-
-    #relationship to Role table
-    roles = relationship("Role", backref="users")  
-    bookings = relationship("Booking", back_populates="users")
-
-class Role(Base):
-    __tablename__ = 'role'
-
-    role_id = Column(Integer, primary_key=True)
-    name = Column(String(100))
+class Pets(PetsBase, table=True):
+    pet_id : int | None = Field(default=None, primary_key=True)
+    owner_id : int = Field(foreign_key="users.id")
+    owner : UsersBase = Relationship(back_populates="pets")
--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,3 +1,51 @@
-from pydantic import BaseModel
+from pydantic import EmailStr, BaseModel, ConfigDict, field_validator
+from sqlmodel import SQLModel
+from enum import Enum
 
+class BaseModel(BaseModel):
+    model_config = ConfigDict(use_enum_values=True)
 
+class SQLModel(SQLModel):
+    model_config = ConfigDict(use_enum_values=True)
+
+class Roles(Enum):
+    ADMIN = "admin"
+    BASIC = "basic"
+
+class UsersBase(SQLModel):
+    full_name : str
+    role : Roles
+    email : EmailStr
+    password : str
+    address : str
+    phone_number : str
+
+class UsersCreate(UsersBase):
+    # this will not be part of the table of users 
+    # this is only used to validate the pets data that will be inputted to 
+    # the pets table. This is attatched here since pets table are dependent from users table.
+    # pets data cannot be initialized on its own since it cannot be created without an owner (or an owner_id since
+    # it is a foreign key)
+
+    # required to have a pet
+    pets : list["PetsBase"]
+
+class PetTypes(Enum):
+    DOG = "dog"
+    CAT = "cat"
+    BIRD = "bird"
+    FISH = "fish"
+    PIG = "pig"
+    HAMSTER = "hamster"
+    HEDGEHOG = "hedgehog"
+    OTHERS = "others"
+
+class PetsBase(SQLModel):
+    @field_validator("type", mode="before")
+    async def lower_case_type(cls, value : str) -> str:
+        return value.lower()
+    
+    pet_name : str
+    age : int 
+    type : PetTypes
+    breed : str

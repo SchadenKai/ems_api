a42c9487cad997d2ba0f06fd239dd0e37c95e092
SchadenKai <kairustecsonpersonal@gmail.com>
2024-07-19 11:03:57 +0800

    added status column and enum type for order and booking table

--- src/booking/schemas.py
+++ src/booking/schemas.py
@@ -1,4 +1,17 @@
-from sqlmodel import SQLModel
+from fastapi import HTTPException, status
+from pydantic import ConfigDict, field_validator
+from sqlmodel import SQLModel, Field
+from enum import Enum
+
+class SQLModelBase(SQLModel):
+    model_config = ConfigDict(use_enum_values=True)
+
+class BookingState(Enum):
+    PENDING = "pending"
+    CONFIRMED = "confirmed"
+    CANCELLED = "cancelled"
+    COMPLETED = "completed"
+
 
 class ServicesBase(SQLModel):
     service_name : str
@@ -6,7 +19,12 @@ class ServicesBase(SQLModel):
     duration : int
 
 class BookingBase(SQLModel):
+    status : BookingState | None = Field(default=BookingState.PENDING)
     total_price : float
 
 class BookingCreate(BookingBase):
-    pass
+    @field_validator("status", mode="before")
+    def validate_status(cls, v : BookingState):
+        if v:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Status cannot be set manually")
+    
--- src/order/schemas.py
+++ src/order/schemas.py
@@ -1,12 +1,32 @@
-from sqlmodel import SQLModel
+from fastapi import HTTPException, status
+from pydantic import ConfigDict, field_validator
+from sqlmodel import SQLModel, Field
+from enum import Enum
+from typing import Optional
+
+class OrderState(Enum):
+    PENDING = "pending"
+    CONFIRMED = "confirmed"
+    CANCELLED = "cancelled"
+    COMPLETED = "completed"
 
 class OrderBase(SQLModel):
+    status : OrderState | None = Field(default=OrderState.PENDING)
     quantity : int 
     total_price : float
 
 # only used for validating the request body
 class OrderCreate(OrderBase):
-    pass
+    order_items : list["OrderItemBase"]
+    @field_validator("status", mode="before")
+    def validate_status(cls, v : OrderState):
+        if v:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Status cannot be set manually")
+    
+class OrderUpdate(OrderBase):
+    status : Optional[OrderState]
+    quantity : Optional[int]
+    total_price : Optional[float]
 
 class OrderItemBase(SQLModel):
     quantity : int
